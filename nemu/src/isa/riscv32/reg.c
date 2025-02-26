1bc7bab98f2008860863de0d4e1be502778d78e0
tracer-ics2024 <tracer@njuics.org>
2025-02-26 10:37:52 +0800

    >  compile NEMU 231220000 叶文玉 Linux yee 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 GNU/Linux  10:37:52 up  1:15,  1 user,  load average: 0.01, 0.01, 0.00

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -25,7 +25,9 @@ const char *regs[] = {
 };
 
 void isa_reg_display() {
-  printf("pc: %x\n",cpu.pc);
+  for (int i=0; i<31; i++) {
+    printf("%s: %x \n",regs[i],cpu.gpr[i]);
+  }
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
