3b7883a4cf2b12c9d89dda3902ca48e85d9748f3
tracer-ics2024 <tracer@njuics.org>
2025-02-27 21:04:03 +0800

    >  compile NEMU 231220000 叶文玉 Linux yee 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 GNU/Linux  21:04:03 up 10:47,  1 user,  load average: 0.19, 0.16, 0.09

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -168,6 +168,7 @@ void sdb_mainloop() {
      * which may need further parsing
      */
     char *args = cmd + strlen(cmd) + 1;
+    printf("%s",args);
     if (args >= str_end) {
       args = NULL;
       // si without args
