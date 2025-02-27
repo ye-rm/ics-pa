7105a9faf897bfec254127970825c5d0db2d93e1
tracer-ics2024 <tracer@njuics.org>
2025-02-27 10:25:44 +0800

    >  compile NEMU 231220000 叶文玉 Linux yee 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 GNU/Linux  10:25:44 up 8 min,  1 user,  load average: 0.03, 0.10, 0.04

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -158,7 +158,7 @@ int get_op(int p,int q){
       pri = TK_MULTI;
     }
   }
-  Log("select op %s at pos %d", tokens[op].str,op+1);
+  Log("select op %s at token no. %d", tokens[op].str,op+1);
   return op;
 }
 
