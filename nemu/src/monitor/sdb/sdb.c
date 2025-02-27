f5e488358c4085bade7eb89653170f1acf5f956f
tracer-ics2024 <tracer@njuics.org>
2025-02-27 21:21:53 +0800

    >  compile NEMU 231220000 叶文玉 Linux yee 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 GNU/Linux  21:21:53 up 11:05,  1 user,  load average: 0.20, 0.11, 0.09

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -124,7 +124,7 @@ static int cmd_x(char* args){
   char *args1=strtok(args," ");
   char *args2=args+strlen(args1)+1;
   if (args1 == NULL||args2 >= args+strlen(args)){
-    cmd_help("x x");
+    printf("check args, refer to 'help x'\n");
     return 0;
   }
   printf("%s\n %s\n",args1,args2);
