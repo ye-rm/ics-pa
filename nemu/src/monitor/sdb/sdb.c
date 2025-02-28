3491d46e53db2d564059bf2304b25d9f8308eba5
tracer-ics2024 <tracer@njuics.org>
2025-02-28 12:16:46 +0800

    >  compile NEMU 231220000 叶文玉 Linux yee 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 GNU/Linux  12:16:46 up  1:59,  1 user,  load average: 0.25, 0.11, 0.03

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -144,7 +144,7 @@ static int cmd_x(char* args){
 
 static int cmd_p(char* args){
   bool success;
-  printf("%u\n",expr(args, &success));
+  printf("0x%x\n",expr(args, &success));
   return 0;
 }
 
