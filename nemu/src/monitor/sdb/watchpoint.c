45954c0a8215d2493192de1adadbedb934124426
tracer-ics2024 <tracer@njuics.org>
2025-03-01 14:58:22 +0800

    >  compile NEMU 231220000 叶文玉 Linux yee 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 GNU/Linux  14:58:22 up  5:04,  1 user,  load average: 0.11, 0.08, 0.02

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -111,6 +111,7 @@ bool check_wp_triggered(){
     if(tmp->initial_val!=vaddr_read(tmp->addr,4)){
       tmp -> triggered = true;
       printf("triggered %d watch point at %x\n",tmp->NO,tmp->addr);
+      free_wp(tmp);
       return true;
     }
   }
