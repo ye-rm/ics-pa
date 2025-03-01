d1e9982c5a6d127ace5eac6dc87d28d202391fb6
tracer-ics2024 <tracer@njuics.org>
2025-03-01 15:09:28 +0800

    >  compile NEMU 231220000 叶文玉 Linux yee 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 GNU/Linux  15:09:28 up  5:15,  1 user,  load average: 0.08, 0.06, 0.02

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -50,7 +50,7 @@ WP* new_wp(uint32_t addr){
     head->addr = addr;
     head->initial_val = vaddr_read(addr,4);
     head->triggered = false;
-    return free_;
+    return head;
   }
   Assert(0,"break points num exceeded %d",NR_WP);
   return NULL;
